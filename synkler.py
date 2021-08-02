e3c56afb231750be096e40b7ab947c538ca4dfcf
Patrick Gillan <pgillan@minorimpact.com>
2021-08-02 10:55:11 -0700

    Changed synkler.py to use the topic exchenge type.

--- synkler.py
+++ synkler.py
@@ -14,54 +14,61 @@ import time
 connection = pika.BlockingConnection(pika.ConnectionParameters(host=config.synkler_server))
 channel = connection.channel()
 
-channel.exchange_declare(exchange='synkler', exchange_type='direct')
-channel.queue_declare(queue='new')
-channel.queue_bind(exchange='synkler', queue='new')
-channel.queue_declare(queue='upload')
-channel.queue_bind(exchange='synkler', queue='upload')
-channel.queue_declare(queue='download')
-channel.queue_bind(exchange='synkler', queue='download')
+channel.exchange_declare(exchange='synkler', exchange_type='topic')
+result = channel.queue_declare(queue='', exclusive=True)
+queue_name = result.method.queue
+channel.queue_bind(exchange='synkler', queue=queue_name, routing_key='new')
+channel.queue_bind(exchange='synkler', queue=queue_name, routing_key='done')
 
 files = {}
-new_files = {}
-def process_new_files(ch, body):
-    file_data = pickle.loads(body)
-    print(file_data)
-    filename = file_data["filename"]
-    new_files[filename] = file_data
-    #if filename in files:
-    #    print("skipping " + filename + " already in the queue")
-    #    return
-
-    #now = datetime.now()
-    #files[filename]  = {"filename":filename, "dest_dir":config.download_dir, "size":0, "md5":None, "state":"uploading", "mtime":now}
-    #print("starting upload for " + filename)
-    #ch.basic_publish(exchange='synkler', routing_key='upload', body=pickle.dumps(files[filename], protocol=4)) #file_data["pickle_protocol"]))
-
 while (True):
     print("new files:")
-    new = channel.basic_get( queue='new', auto_ack=True)
-    while new[2] != None:
-        process_new_files(channel, new[2])
-        new = channel.basic_get( queue='new', auto_ack=True)
+    method, properties, body = channel.basic_get( queue=queue_name, auto_ack=True)
+    while body != None:
+        routing_key = method.routing_key
+        file_data = pickle.loads(body)
+        print(file_data)
+        f = file_data["filename"]
+        if (routing_key == "new"):
+            if (f not in files):
+                print(f"setting {f} state to 'upload'")
+                files[f] = {"filename":f, "dir":config.download_dir, "size":0, "mtime":None, "md5":None, "state":"upload"}
+            elif (files[f]["size"] == file_data["size"]): # and files[f]["mtime"] == file_data["mtime"] and files[f]["md5"] == file_data["md5"]):
+                print(f"setting {f} state to 'download'")
+                files[f]["state"] = "download"
+        elif (routing_key == "done"):
+            # TODO: compare the specs (md5, etc) and make sure the new file matches the local file.
+            # TODO: file is done, like, delete it, or whatever.
+            files[f]["state"] = "done"
+        method, properties, body = channel.basic_get( queue=queue_name, auto_ack=True)
 
     for f in os.listdir(config.download_dir):
         if (re.search("^\.", f)):
             continue
-        if (f not in new_files):
+        if (f not in files):
             # TODO: delete these files from the download directory after X minutes have passed -- there's a delay
             #   during the upload.py startup that would cause these to be deleted as soon as this script starts and
-            #   then they'd be uploaded again a few seconds later.
-            print("DELETE:" + f)
+            #   then they'd be uploaded again a few seconds later... maybe?
+            print("DELETE:" + f + "?")
             continue
 
         size = minorimpact.dirsize(config.download_dir + "/" + f)
         mtime = os.path.getmtime(config.download_dir + "/" + f)
         if (f in files):
+            if (files[f]["state"] == "done"):
+                print(f"DONE: {f}")
+                # TODO: Delete the local files once they're marked done.
+                print(f"DELETE: {f}")
+                continue
+            if (files[f]["state"] == "download"):
+                continue
+
             if (size == files[f]["size"] and files[f]["mtime"] == mtime):
+                # The file has stopped changing, we can assume it's no longer being written to -- grab the md5sum.
                 if (files[f]["md5"] == None):
                     md5 = minorimpact.md5dir(config.download_dir + "/" + f)
                     files[f]["md5"] = md5
+                    # TODO: compare this, make sure it's right
                     #if (f in new_files):
                     #    if (size == new_files[f]["size"] and mtime == new_files[f]["mtime"] and md5 == new_files[f]["md5"]):
                     #        files[f]["state"] = "error"
@@ -71,21 +78,20 @@ while (True):
                 files[f]["size"] = size
                 files[f]["mtime"] = mtime
         else:
-            files[f]  = {"filename":f, "dir":config.download_dir, "size":0, "md5":None, "mtime":None}
-
-    for f in new_files:
-        # For now we're just auto adding anything in the new_files list to the upload queue, but at some point we need to check and make sure
-        #   we've got space before we just blindly starting pulling everyting under the sun.
-        if (f not in files):
-            # TODO: confirm there's enough space to handle these new files before we sat yes to an upload.
-            files[f]  = {"filename":f, "dest_dir":config.download_dir, "size":0, "md5":None, "mtime":None}
+            # Not actually sure what to do with files that exist here but are not coming at us from the upload
+            #   server.  Delete them? download them just to be on the safe side?
+            md5 = minorimpact.md5dir(config.download_dir + "/" + f)
+            files[f]  = {"filename":f, "dir":config.download_dir, "size":size, "md5":md5, "mtime":mtime, "state":"unknown"}
 
     for f in files:
-        if (f in new_files):
-            print("upload " + f)
+        if (files[f]["state"] == "upload"):
+            # TODO: the upload script blocks while running the rsync, so this script just pumps out command after command -- causing
+            #   the upload script to eventually execute a shitload of redundant rsync commands.  It still "works", but it's
+            #   shit.
+            print(f"uploading {files[f]}")
             channel.basic_publish(exchange='synkler', routing_key='upload', body=pickle.dumps(files[f], protocol=4))
-        if (files[f]['md5'] is not None):
-            print("download " + f)
+        elif (files[f]["state"] == "download"):
+            print(f"downloading {files[f]}")
             channel.basic_publish(exchange='synkler', routing_key='download', body=pickle.dumps(files[f], protocol=4))
 
     print("\n")
