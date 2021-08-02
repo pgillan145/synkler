35bbb42af765b6f514e88281ce98a3dc9da3c329
Patrick Gillan <pgillan@minorimpact.com>
2021-08-02 13:54:21 -0400

    Changed upload script to use the 'topic' exchange.

--- sample-config.py
+++ sample-config.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+data_dir = "./synkler-data"
 file_dir = "./downloads"
 rsync = "/usr/bin/rsync"
 synkler_server = "localhost"
--- upload.py
+++ upload.py
@@ -18,15 +18,20 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(host=config.synkl
 channel = connection.channel()
 
 # message queues
-channel.exchange_declare(exchange='synkler', exchange_type='direct')
-channel.queue_declare(queue='new')
-channel.queue_bind(exchange='synkler', queue='new')
+channel.exchange_declare(exchange='synkler', exchange_type='topic')
+result = channel.queue_declare(queue='', exclusive=True)
+queue_name = result.method.queue
+channel.queue_bind(exchange='synkler', queue=queue_name, routing_key='done')
+channel.queue_bind(exchange='synkler', queue=queue_name, routing_key='upload')
 
 files = {}
+#data_file = config.data_dir + "/files.pickle"
+#if (os.path.exists(data_file)):
+#   files = pickle.load(open(data_file, "rb"))
 
 while True:
 # collect all the files that need to be uploaded
-    print("new files:")
+    print("my files:")
     churn = True
     while churn:
         churn = False
@@ -36,76 +41,78 @@ while True:
             size = minorimpact.dirsize(upload_dir + "/" + f)
             mtime = os.path.getmtime(upload_dir + "/" + f) 
             if f in files:
+                if files[f]['state'] != 'new':
+                    continue
                 if mtime == files[f]['mtime'] and size == files[f]['size']:
                     # Don't md5 the file until we know the file has stopped being written to, just in case
                     if files[f]['md5'] is None:
                         md5 = minorimpact.md5dir(upload_dir + "/" + f)
                         files[f]['md5'] = md5
-
                 else:
                     print(f"mtime({mtime}) and size({size}) aren't settled for {f}({files[f]['mtime']},{files[f]['size']})")
+                    files[f]['mtime'] = mtime
+                    files[f]['size'] = size
                     churn = True
-                files[f]['mtime'] = mtime
-                files[f]['size'] = size
             else:
                 # TODO: Apparently the server is going to have to tell us what pickle protocols it knows.  This machine supports '5',
                 #   but the server only supports '4'...
-                files[f] = {'filename':f, 'pickle_protocol':4, 'mtime':mtime, 'size':size, 'state':'new', 'md5':None}
+                files[f] = {'filename':f, 'pickle_protocol':4, 'mtime':mtime, 'size':size, 'state':'new', 'md5':None, "dir":upload_dir}
                 churn = True
         time.sleep(1)
 
     for f in files:
-        print(files[f])
-        channel.basic_publish(exchange='synkler', routing_key='new', body=pickle.dumps(files[f]))
+        if (files[f]["state"] == "new"):
+            print(files[f])
+            channel.basic_publish(exchange='synkler', routing_key=files[f]['state'], body=pickle.dumps(files[f]))
 
     # Pull the list of files on the middle upload server
     print("uploads:")
-    channel.queue_declare(queue='upload')
-    channel.queue_bind(exchange='synkler', queue='upload')
-    upload = channel.basic_get( 'upload', True)
-    while upload[2] != None:
-        file_data = pickle.loads(upload[2])
-        print(file_data)
+    method,properties,body = channel.basic_get( queue_name, True)
+    while body != None:
+        routing_key = method.routing_key
+        file_data = pickle.loads(body)
         filename = file_data['filename']
-        dest_dir = file_data['dir']
+        if filename not in files:
+            continue
         md5 = file_data['md5']
         size = file_data['size']
         mtime = file_data['mtime']
-        if filename in files: 
-            if files[filename]['md5'] != md5 or files[filename]['size'] != size:
+        if (routing_key == "done"):
+            if files[filename]['md5'] == md5 and files[filename]['size'] == size and files[filename]["mtime"] == mtime and files[filename]["state"] != "done":
+                # TODO: Move or delete this file or whatever.
+                print(f"DONE {filename}")
+                files[filename]["state"] = "done"
+            else:
+                # TODO: start including the hostname in file_data so I know what server is actuall sending these messages.
+                print(f"ERROR: {filename} on final destination doesn't match")
+                files[filename]["state"] = "error"
+        elif (routing_key == "upload"):
+            if (files[filename]["state"] != "new"):
+                #print(f"{filename} status is {files[filename]['state']}")
+                continue
+#        if (files[filename]['md5'] == md5 and files[filename]['size'] == size and files[filename]["mtime"] == mtime):
+#                print(f"uploaded {filename}")
+#                files[filename]["state"] = "uploaded"
+#                continue
+            dest_dir = None
+            if ("dest_dir" in file_data):
+                dest_dir = file_data['dest_dir']
+            else:
+                dest_dir = file_data['dir']
+
+            if dest_dir is not None:
                 print("  uploading " + filename)
                 rsync_command = [rsync, "--archive", "--partial", upload_dir + "/" + filename, config.synkler_server + ":" + dest_dir + "/"]
                 print(' '.join(rsync_command))
-
                 return_code = subprocess.call(rsync_command)
                 print("Output: ", return_code)
 
         # get the next file from the queue
-        upload = channel.basic_get('upload', True)
+        method,properties,body = channel.basic_get( queue_name, True)
 
-
-    # pull from the queue of files that are verified on the server and 
-    print("done:")
-    channel.queue_declare(queue='done')
-    channel.queue_bind(exchange='synkler', queue='done')
-    done = channel.basic_get('done', True)
-    while done[2] != None:
-        # archive the file that's being requested.
-        file_data = pickle.loads(done[2])
-        print(file_data)
-        filename = file_data["filename"]
-        md5 = file_data['md5']
-        size = file_data['size']
-        mtime = file_data['mtime']
-        if filename in files and files[filename]['md5'] == md5 and files[filename]['size'] == size:
-            # TODO: This file is confirmed downloaded, do what we have to do to move it out of this directory
-            #   and into its next phase of existence
-            print("DONE " + filename)
-
-        # tell uTorrent to move the file to 'Media/Downloads'.  Not sure how that interface will work.
-        # get the next file from the queue
-        done = channel.basic_get( 'done', True)
     print("\n")
+    #data_file = config.data_dir + "/files.pickle"
+    #pickle.dump(files, open(data_file, "wb"))
     time.sleep(5)
 
 
