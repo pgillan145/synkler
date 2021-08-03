89af204c449c05a04e8c529299d21c8949f26567
Patrick Gillan <pgillan@minorimpact.com>
2021-08-03 17:15:27 -0400

    reduced verbosity so I can run in verbose mode lt and not get spammed.

--- download.py
+++ download.py
@@ -31,7 +31,7 @@ channel.queue_bind(exchange='synkler', queue=queue_name, routing_key='download')
 
 files = {}
 while True:
-    if (args.verbose): print(f"checking {download_dir}")
+    #if (args.verbose): print(f"checking {download_dir}")
     churn = True
     while churn:
         churn = False
@@ -45,13 +45,14 @@ while True:
                     if (files[f]["md5"] is None):
                         md5 = minorimpact.md5dir(config.download_dir + "/" + f)
                         files[f]["md5"] = md5
+                        if (args.verbose): print(f"{f} md5:{md5}")
                 else:
                     churn = True
                     files[f]["size"] = size
                     files[f]["mtime"] = mtime
         time.sleep(1)
 
-    if (args.verbose): print("checking for synkler commands")
+    #if (args.verbose): print("checking for synkler commands")
     method, properies, body = channel.basic_get( queue_name, True)
     while body != None:
         file_data = pickle.loads(body)
@@ -61,6 +62,7 @@ while True:
         size = file_data["size"]
         mtime = file_data["mtime"]
         if (f not in files):
+            if (args.verbose): print(f"new file:{f}")
             files[f]  = {"filename":f, "size":0, "md5":None, "mtime":0, "dir":config.download_dir, "state":"download"}
         elif (files[f]["size"] != size or  md5 != files[f]["md5"] or files[f]["mtime"] != mtime):
             rsync_command = [rsync, "--archive", "--partial", synkler_server + ":\"" + dl_dir + "/" + f + "\"", download_dir + "/"]
@@ -69,16 +71,17 @@ while True:
             return_code = subprocess.call(rsync_command)
             if (args.verbose): print("Output: ", return_code)
         else:
+            if (args.verbose): print(f"{f} done")
             files[f]["state"] = "done"
         # get the next file from the queue
         method, properies, body = channel.basic_get( queue_name, True)
 
-    if (args.verbose): print("reporting download status to synkler")
+    #if (args.verbose): print("reporting download status to synkler")
     for f in files:
         if (files[f]["state"] == "done"):
-            if (args.verbose): print(f"{f} done")
+            #if (args.verbose): print(f"{f} done")
             channel.basic_publish(exchange='synkler', routing_key='done', body=pickle.dumps(files[f]))
-    if (args.verbose): print("\n")
+    #if (args.verbose): print("\n")
 
     # TODO: Figure out how to clear a file out of the list once it's finished and removed
     #   from them upload server.  I can use 'mtime' remove anything that hasn't been updated in like half an hour,,
