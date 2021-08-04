c2eb2cb214a9c7e414d7421293d479b8c391f01a
Patrick Gillan <pgillan@minorimpact.com>
2021-08-04 15:55:17 -0400

    Reduced verbosity.

--- upload.py
+++ upload.py
@@ -15,13 +15,10 @@ import time
 upload_dir = config.file_dir
 rsync = config.rsync
 
+# TODO: Add an ID argument so different multiple synkler sets can be run simultaneously
 parser = argparse.ArgumentParser(description="Monitor directory and initiate synkler transfers")
-#parser.add_argument('filename', nargs="?")
 parser.add_argument('--verbose', help = "extra loud output", action='store_true')
-#parser.add_argument('--logging', help = "turn on logging", action='store_true')
 args = parser.parse_args()
-
-
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host=config.synkler_server))
 channel = connection.channel()
@@ -37,7 +34,7 @@ files = {}
 
 while True:
 # collect all the files that need to be uploaded
-    if (args.verbose): print(f"checking {upload_dir}")
+    #if (args.verbose): print(f"checking {upload_dir}")
     churn = True
     while churn:
         churn = False
@@ -53,8 +50,9 @@ while True:
                         if files[f]['md5'] is None:
                             md5 = minorimpact.md5dir(upload_dir + "/" + f)
                             files[f]['md5'] = md5
+                            if (args.verbose): print(f"{f} md5:{md5}")
                     else:
-                        if (args.verbose): print(f"mtime({mtime}) and size({size}) aren't settled for {f}({files[f]['mtime']},{files[f]['size']})")
+                        #if (args.verbose): print(f"mtime({mtime}) and size({size}) aren't settled for {f}({files[f]['mtime']},{files[f]['size']})")
                         files[f]['mtime'] = mtime
                         files[f]['size'] = size
                         churn = True
@@ -71,7 +69,7 @@ while True:
             channel.basic_publish(exchange='synkler', routing_key=files[f]['state'], body=pickle.dumps(files[f]))
 
     # Pull the list of files on the middle upload server
-    if (args.verbose): print("checking for synkler commands")
+    #if (args.verbose): print("checking for synkler commands")
     method,properties,body = channel.basic_get( queue_name, True)
     while body != None:
         routing_key = method.routing_key
@@ -84,19 +82,16 @@ while True:
             if (routing_key == "done"):
                 if (files[f]["state"] == "new"):
                     if files[f]['md5'] == md5 and files[f]['size'] == size and files[f]["mtime"] == mtime:
-                        # TODO: Move or delete this file or whatever.
-                        if (args.verbose): print(f"DONE {f}")
+                        if (args.verbose): print(f"{f} done")
                         files[f]["state"] = "done"
                         if (config.cleanup_script is not None):
                             command = config.cleanup_script.split(" ")
                             for i in range(len(command)):
                                 if command[i] == "%f":
                                     command[i] = f
-                                    # Not sure if I need the quotes, subprocess might handle that for me.
-#command[i] = "\"" + command[i] + "\""
                             if (args.verbose): print(' '.join(command))
                             return_code = subprocess.call(command)
-                            if (args.verbose): print("Output: ", return_code)
+                            #if (args.verbose): print("Output: ", return_code)
                     else:
                         # TODO: start including the hostname in file_data so I know what server is actuall sending these messages.
                         if (args.verbose): print(f"ERROR: {f} on final destination doesn't match")
@@ -113,16 +108,20 @@ while True:
                         dest_dir = file_data['dir']
 
                     if dest_dir is not None and (files[f]['md5'] != md5 or files[f]['size'] != size or files[f]["mtime"] != mtime):
-                        if (args.verbose): print("  uploading " + f)
+                        #if (args.verbose): print("  uploading " + f)
                         rsync_command = [rsync, "--archive", "--partial", upload_dir + "/" + f, config.synkler_server + ":" + dest_dir + "/"]
                         if (args.verbose): print(' '.join(rsync_command))
                         return_code = subprocess.call(rsync_command)
-                        if (args.verbose): print("Output: ", return_code)
+                        #if (args.verbose): print("Output: ", return_code)
+                        if (return_code == 0):
+                            files[f]["size"] = minorimpact.dirsize(upload_dir + "/" + f)
+                            files[f]["mtime"] = os.path.getmtime(upload_dir + "/" + f)
+                            files[f]["md5"] = minorimpact.md5dir(upload_dir + "/" + f)
 
         # get the next file from the queue
         method,properties,body = channel.basic_get( queue_name, True)
 
-    if (args.verbose): print("\n")
+    #if (args.verbose): print("\n")
     time.sleep(5)
 
 
