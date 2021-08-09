70d05c28accec8039fafb3c43460d7f233d0aa28
Patrick Gillan <patrick@minorimpact.com>
2021-08-09 02:19:02 -0400

    Added --id option

--- download.py
+++ download.py
@@ -12,12 +12,13 @@ import re
 import subprocess
 import time
 
-download_dir = config.download_dir
+download_dir = config.file_dir
 synkler_server = config.synkler_server
 rsync = config.rsync
 
 parser = argparse.ArgumentParser(description="Synkler download script")
-parser.add_argument('--verbose', help = "extra loud output", action='store_true')
+parser.add_argument('-v', '--verbose', help = "extra loud output", action='store_true')
+parser.add_argument('--id', nargs='?', help = "id of a specific synkler group", default="default")
 args = parser.parse_args()
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host=synkler_server))
@@ -27,7 +28,7 @@ channel = connection.channel()
 channel.exchange_declare(exchange='synkler', exchange_type='topic')
 result = channel.queue_declare(queue='', exclusive=True)
 queue_name = result.method.queue
-channel.queue_bind(exchange='synkler', queue=queue_name, routing_key='download')
+channel.queue_bind(exchange='synkler', queue=queue_name, routing_key='download.' + args.id)
 
 files = {}
 while True:
@@ -38,12 +39,12 @@ while True:
         for f in os.listdir(download_dir):
             if (re.search("^\.", f)):
                 continue
-            size = minorimpact.dirsize(config.download_dir + "/" + f)
-            mtime = os.path.getmtime(config.download_dir + "/" + f)
+            size = minorimpact.dirsize(config.file_dir + "/" + f)
+            mtime = os.path.getmtime(config.file_dir + "/" + f)
             if (f in files):
                 if (size == files[f]["size"] and files[f]["mtime"] == mtime):
                     if (files[f]["md5"] is None):
-                        md5 = minorimpact.md5dir(config.download_dir + "/" + f)
+                        md5 = minorimpact.md5dir(config.file_dir + "/" + f)
                         files[f]["md5"] = md5
                         if (args.verbose): print(f"{f} md5:{md5}")
                 else:
@@ -64,16 +65,16 @@ while True:
         mtime = file_data["mtime"]
         if (f not in files):
             if (args.verbose): print(f"new file:{f}")
-            files[f]  = {"filename":f, "size":0, "md5":None, "mtime":0, "dir":config.download_dir, "state":"download"}
+            files[f]  = {"filename":f, "size":0, "md5":None, "mtime":0, "dir":config.file_dir, "state":"download"}
 
         if (files[f]["size"] != size or md5 != files[f]["md5"] or files[f]["mtime"] != mtime):
             rsync_command = [rsync, "--archive", "--partial", synkler_server + ":\"" + dl_dir + "/" + f + "\"", download_dir + "/"]
             if (args.verbose): print(' '.join(rsync_command))
             return_code = subprocess.call(rsync_command)
             if (return_code == 0):
-                files[f]["size"] = minorimpact.dirsize(config.download_dir + "/" + f)
-                files[f]["mtime"] = os.path.getmtime(config.download_dir + "/" + f)
-                files[f]["md5"] = minorimpact.md5dir(config.download_dir + "/" + f)
+                files[f]["size"] = minorimpact.dirsize(config.file_dir + "/" + f)
+                files[f]["mtime"] = os.path.getmtime(config.file_dir + "/" + f)
+                files[f]["md5"] = minorimpact.md5dir(config.file_dir + "/" + f)
             elif (args.verbose): print("Output: ", return_code)
         else:
             if (files[f]["state"] != "done"):
@@ -86,7 +87,7 @@ while True:
     for f in filenames:
         if (files[f]["state"] == "done"):
             if (args.verbose): print(f"{f} done")
-            channel.basic_publish(exchange='synkler', routing_key='done', body=pickle.dumps(files[f]))
+            channel.basic_publish(exchange='synkler', routing_key='done.' + args.id, body=pickle.dumps(files[f]))
             del files[f]
     #if (args.verbose): print("\n")
 
