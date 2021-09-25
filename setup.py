d067b4f45c7586596610d7d1cfb13e4c279660ff
Patrick Gillan <patrick@minorimpact.com>
2021-09-25 02:04:20 -0400

    Moved code into a package, added a setup script.

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
+#!/usr/bin/env python3
+
 from setuptools import find_packages, setup
+import synkler
 
 setup(
     name='synkler',
-    #packages=find_packages(include=['synkler']),
-    version='0.0.1',
     description="A three-body rsync solution.",
+    packages=find_packages(include=['synkler']),
     author="Patrick Gillan",
     author_email = "pgillan@minorimpact.com",
-    license='GPLv3',
+    entry_points = { "console_scripts": [ "synkler = synkler:main" ] },
     install_requires=['minorimpact', 'pika'],
+    license='GPLv3',
     setup_requires=[],
     tests_require=[],
-    url="https://github.com/pgillan145/synkler"
+    url="https://github.com/pgillan145/synkler",
+    version=synkler.__version__
 )
